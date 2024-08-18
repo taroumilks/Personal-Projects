b600710d9eb036834552aebdbc9b700781006c32
taroumilks <112773673+taroumilks@users.noreply.github.com>
2024-08-17 21:26:30 -0500

    Test

---
+++ Test/project based off original code/Python Folder/main.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask, session, url_for, request, redirect
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+
+client_id = ''
+client_secret = ''
+redirect_uri = 'http://localhost:5000/callback'
+scope = 'playlist-read-private'
+
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id = client_id,
+    client_secret = client_secret,
+    redirect_uri = redirect_uri,
+    scope = scope,
+    cache_handler = cache_handler,
+    show_dialog = True
+)
+
+sp = Spotify(auth_manager=sp_oauth)
+
+@app.route('/')
+def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_playlists'))
+
+# Callback endpoint
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
+
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    #Token is Valid, Extract Playlist names, urls, and print out.
+    playlists = sp.current_user_playlists()
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+
+    return playlists_html
+
+#Logout, Clear the Session
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
+
